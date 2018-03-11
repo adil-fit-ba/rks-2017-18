f428e393a2f34c515664ff549bc85848c84f1723
Adil Joldic <adil@edu.fit.ba>
2018-03-12 00:59:01 +0100

    aa

---
+++ kodovi/Projekat/app/src/main/java/ba/fit/app/hci_odbrana/MainActivity.java
@@ -0,0 +1,22 @@
+package ba.fit.app.hci_odbrana;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import ba.fit.app.hci_odbrana.helper.Util;
+import ba.fit.app.hci_odbrana.podaci.Storage;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       
+
+    }
+}
